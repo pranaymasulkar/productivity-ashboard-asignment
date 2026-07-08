2132715669bb2c34996b52b0ff5e1d0b54d7c9cf
vickymasulkar@gmail.com <vickymasulkar@gmail.com>
2026-07-08 20:28:56 +0530

    created crud operation for todo, daily plan, daily goals and motivation qouted

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ const CLOSE_DAILY_GOAL_MODAL = document.querySelector("#closeDailyGoalsModal");
 // ================================================================================
 
 const TODOS_DATA = JSON.parse(localStorage.getItem("todos")) || [];
+const DAILY_PLAN_DATA = JSON.parse(localStorage.getItem("daily_plans")) || [];
+const DAILY_GOALS_DATA = JSON.parse(localStorage.getItem("goals")) || [];
 
 // ================================================================================
 // Page Navigation Logis
@@ -96,7 +98,7 @@ function todoUI(data) {
           <td><strong>${item.description}</strong></td>
           <td><span class="categry">${item.category}</span></td>
           <td><strong>${item.priority}</strong></td>
-          <td><strong>${item.due_date}</strong></td>
+          <td><strong>${item.duedate}</strong></td>
           <td>
             <button class="edit-btn" onclick="editTodo(${item.id})">
             <i class="ri-pencil-fill"></i>
@@ -112,35 +114,38 @@ function todoUI(data) {
 
 TODO_FORM.addEventListener("submit", (e) => {
   e.preventDefault();
-  const inputData = new FormData(e.target);
 
-  const title = inputData.get("title");
-  const description = inputData.get("description");
-  const category = inputData.get("category");
-  const priority = inputData.get("priority");
-  const due_date = inputData.get("due_date");
+  const TODO_TITLE = document.getElementById("todoTitle");
+  const TODO_DESCRIPTION = document.getElementById("todoDescription");
+  const TODO_CATEGORY = document.getElementById("todoCategory");
+  const TODO_PRIORITY = document.getElementById("todoPriority");
+  const TODO_DUEDATE = document.getElementById("todoDuedate");
 
-  console.log(priority);
+  let todoTitle = TODO_TITLE.value;
+  let todoDescription = TODO_DESCRIPTION.value;
+  let todoCategory = TODO_CATEGORY.value;
+  let todoPriority = TODO_PRIORITY.value;
+  let todoDuedate = TODO_DUEDATE.value;
+
   let newTodo = {
     id: Date.now(),
-    title,
-    description,
-    category,
-    priority,
-    due_date,
+    title: todoTitle,
+    description: todoDescription,
+    category: todoCategory,
+    priority: todoPriority,
+    duedate: todoDuedate,
     status: false,
   };
+  if (editedTodoId !== null) {
+    const todo = TODOS_DATA.find((todo) => todo.id === editedTodoId);
+    todo.title = todoTitle;
+    todo.description = todoDescription;
+    todo.category = todoCategory;
+    todo.priority = todoPriority;
+    todo.duedate = todoDuedate;
 
-  if (editedTodoId) {
-    const todo = TODOS_DATA.find((id) => id.id === editedTodoId);
-    todo.title = title;
-    todo.description = description;
-    todo.category = category;
-    todo.priority = priority;
-    todo.due_date = due_date;
-
-    alert("Todo Updated");
     editedTodoId = null;
+    alert("Todo Updated");
   } else {
     TODOS_DATA.push(newTodo);
     alert("Todo Created");
@@ -149,22 +154,261 @@ TODO_FORM.addEventListener("submit", (e) => {
   localStorage.setItem("todos", JSON.stringify(TODOS_DATA));
   ADD_TODO_MODAL.style.display = "none";
   TODO_FORM.reset();
-  console.log(TODOS_DATA, "ye hai element");
   todoUI(TODOS_DATA);
 });
 
-todoUI(TODOS_DATA);
+// =================================
+// Edit Todo Logic
+// =================================
 
 let editedTodoId = null;
 
 function editTodo(id) {
   ADD_TODO_MODAL.style.display = "flex";
   const todo = TODOS_DATA.find((todo) => todo.id === id);
-  editedTodoId = todo;
+  editedTodoId = id;
 
-  title.value = todo.title;
-  description.value = todo.description;
-  category.value = todo.description;
-  priority.value = todo.priority;
-  due_date.value = todo.due_date;
+  todoTitle.value = todo.title;
+  todoDescription.value = todo.description;
+  todoCategory.value = todo.category;
+  todoPriority.value = todo.priority;
+  todoDuedate.value = todo.duedate;
 }
+
+// =================================
+// Delete Todo Logic
+// =================================
+
+function deleteTodo(id) {
+  const todos = TODOS_DATA.filter((todo) => todo.id !== id);
+  console.log(todos);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  todoUI(todos);
+}
+
+// ================================================================================
+// Daily Plan UI Logic
+// ================================================================================
+
+const DAILY_PLAN_LIST_TABLE = document.querySelector("#dailyPlanListTable");
+function planUI(data) {
+  DAILY_PLAN_LIST_TABLE.innerHTML = "";
+  data.forEach((item, index) => {
+    return (DAILY_PLAN_LIST_TABLE.innerHTML += `
+     <tr>
+          <td>${index + 1}</td>
+          <td><strong>${item.date}</strong></td>
+          <td>${item.title}</td>
+          <td><strong>${item.description}</strong></td>
+          <td><span class="categry">${item.category}</span></td>
+          <td><strong>${item.priority}</strong></td>
+          <td><strong>${item.date}</strong></td>
+          <td>
+            <button class="edit-btn" onclick="editPlan(${item.id})">
+            <i class="ri-pencil-fill"></i>
+            </button>
+            <button class="delete-btn" onclick="deletePlan(${item.id})">
+            <i class="ri-delete-bin-4-fill"></i>
+            </button>
+          </td>
+        </tr>
+  `);
+  });
+}
+
+// ================================================================================
+// Add Daily Plan Logic
+// ================================================================================
+
+const DAILY_PLAN_FORM = document.querySelector("#dailyPlanForm");
+
+DAILY_PLAN_FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const PLAN_TITLE = document.getElementById("planTitle");
+  const PLAN_DESCRIPTION = document.getElementById("planDescription");
+  const PLAN_CATEGORY = document.getElementById("planCategory");
+  const PLAN_PRIORITY = document.getElementById("planPriority");
+  const PLAN_DATE = document.getElementById("planDate");
+  const PLAN_TIME = document.getElementById("planTime");
+
+  let planTitle = PLAN_TITLE.value;
+  let planDescription = PLAN_DESCRIPTION.value;
+  let planCategory = PLAN_CATEGORY.value;
+  let planPriority = PLAN_PRIORITY.value;
+  let planDate = PLAN_DATE.value;
+  let planTime = PLAN_TIME.value;
+
+  let newPlan = {
+    id: Date.now(),
+    title: planTitle,
+    description: planDescription,
+    category: planCategory,
+    priority: planPriority,
+    date: planDate,
+    time: planTime,
+    status: false,
+  };
+
+  if (editPlanId !== null) {
+    const plan = DAILY_PLAN_DATA.find((plan) => plan.id === editPlanId);
+
+    plan.title = title;
+    plan.description = description;
+    plan.category = category;
+    plan.priority = priority;
+    plan.date = date;
+    plan.time = time;
+
+    alert("Plan Update.");
+    editPlanId = null;
+  } else {
+    alert("Plan Added.");
+  }
+
+  DAILY_PLAN_DATA.push(newPlan);
+  localStorage.setItem("daily_plans", JSON.stringify(DAILY_PLAN_DATA));
+  ADD_DAILY_PLAN_MODAL.style.display = "none";
+  DAILY_PLAN_FORM.reset();
+  planUI(DAILY_PLAN_DATA);
+});
+
+// =================================
+// Edit Daily Plan Logic
+// =================================
+let editPlanId = null;
+
+function editPlan(id) {
+  ADD_DAILY_PLAN_MODAL.style.display = "flex";
+  console.log(id);
+  const plan = DAILY_PLAN_DATA.find((plan) => plan.id === id);
+  editPlanId = id;
+
+  planTitle.value = plan.title;
+  planDescription.value = plan.description;
+  planCategory.value = plan.category;
+  planPriority.value = plan.priority;
+  planDate.value = plan.date;
+  planTime.value = plan.time;
+}
+
+// =================================
+// Delete Daily Plan Logic
+// =================================
+
+function deletePlan(id) {
+  const plans = DAILY_PLAN_DATA.filter((plan) => plan.id !== id);
+  console.log(plans);
+  localStorage.setItem("daily_plans", JSON.stringify(plans));
+  planUI(plans);
+}
+
+// ================================================================================
+// Daily Goals Add Logic
+// ================================================================================
+
+const DAILY_GOALS_LIST_TABLE = document.querySelector("#dailyGoalsListTable");
+
+function goalUI(data) {
+  DAILY_GOALS_LIST_TABLE.innerHTML = "";
+  data.forEach((item, index) => {
+    return (DAILY_GOALS_LIST_TABLE.innerHTML += `
+      <tr>
+          <td>${index + 1}</td>
+          <td>${index + 1}</td>
+          <td>${item.title}</td>
+          <td><strong>${item.description}</strong></td>
+          <td>
+            <button class="edit-btn" onclick="editGoal(${item.id})">
+            <i class="ri-pencil-fill"></i>
+            </button>
+            <button class="delete-btn" onclick="deleteGoal(${item.id})">
+            <i class="ri-delete-bin-4-fill"></i>
+            </button>
+          </td>
+        </tr>
+    `);
+  });
+}
+
+// ================================================================================
+// Daily Goals Add Logic
+// ================================================================================
+
+const DAILY_GOALS_FORM = document.querySelector("#dailyGoalsForm");
+
+DAILY_GOALS_FORM.addEventListener("submit", (e) => {
+  ADD_DAILY_GOAL_MODAL.style.display = "flex";
+  const GOAL_TITLE = document.getElementById("goalTitle");
+  const GOAL_DESCRIPTION = document.getElementById("goalDescription");
+
+  let goalTitle = GOAL_TITLE.value;
+  let goalDescription = GOAL_DESCRIPTION.value;
+
+  let newGoal = {
+    id: Date.now(),
+    title: goalTitle,
+    description: goalDescription,
+    status: false,
+  };
+
+  if (editGoalId !== null) {
+    const goal = DAILY_GOALS_DATA.find((goal) => goal.id === editGoalId);
+
+    goal.title = title;
+    goal.description = description;
+
+    editGoalId = null;
+    alert("Goal Update.");
+  } else {
+    DAILY_GOALS_DATA.push(newGoal);
+    alert("Goal Added.");
+  }
+
+  localStorage.setItem("goals", JSON.stringify(DAILY_GOALS_DATA));
+  ADD_DAILY_GOAL_MODAL.style.display = "none";
+  DAILY_GOALS_FORM.reset();
+  goalUI(DAILY_GOALS_DATA);
+});
+
+// =================================
+// Edit Daily Plan Logic
+// =================================
+
+let editGoalId = null;
+
+function editGoal(id) {
+  ADD_DAILY_GOAL_MODAL.style.display = "flex";
+  const goal = DAILY_GOALS_DATA.find((goal, item) => goal.id === id);
+  editGoalId = id;
+  goalTitle.value = goal.time;
+  goalDescription.value = goal.description;
+}
+
+// =================================
+// Delete Daily Plan Logic
+// =================================
+
+function deleteGoal(id) {
+  const goals = DAILY_GOALS_DATA.filter((goal) => goal.id !== id);
+  localStorage.setItem("goals", JSON.stringify(goals));
+}
+
+// ================================================================================
+// Initially Renderd Function
+// ================================================================================
+
+const url = "https://zenquotes.io/api/random";
+
+async function fetchQuates() {
+  const response = await fetch(url);
+  console.log(response, "there are qoutes");
+}
+fetchQuates();
+// ================================================================================
+// Initially Renderd Function
+// ================================================================================
+
+goalUI(DAILY_GOALS_DATA);
+todoUI(TODOS_DATA);
+planUI(DAILY_PLAN_DATA);
